c8b8f48600334344a33b853ee60deed46d67fde1
andrels2602 <lolandrewrofl@gmail.com>
2025-11-15 04:12:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# create drt vid
+sudo mkdir -p /usr/share/ps1boot
+# copy video
+sudo cp boot.mp4 /usr/share/ps1boot/boot.mp4
+# copy script
 sudo cp ps1boot.sh /usr/local/bin/
 sudo chmod +x /usr/local/bin/ps1boot.sh
-sudo cp ps1boot@.service /etc/systemd/system/
-echo "Installed! To use:"
-echo "  sudo systemctl enable ps1boot@/path/to/video.mp4.service"
+# copy service
+sudo cp ps1boot.service /etc/systemd/system/
+# enable
+sudo systemctl enable ps1boot.service
+
+echo "ps1boot vid installed reboot!"
