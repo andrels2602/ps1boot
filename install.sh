293c48e89e3a1ff10db816439963f6ea45758623
andrels2602 <lolandrewrofl@gmail.com>
2025-11-15 04:22:29 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ sudo cp boot.mp4 /usr/share/ps1boot/boot.mp4
 sudo cp ps1boot.sh /usr/local/bin/
 sudo chmod +x /usr/local/bin/ps1boot.sh
 
-# copy systemd template
-sudo cp ps1boot@.service /etc/systemd/system/
+# copy service
+sudo cp ps1boot.service /etc/systemd/system/
 
-# enable service with the boot video path (no .service at the end)
-sudo systemctl enable "ps1boot@/usr/share/ps1boot/boot.mp4"
+# enable service
+sudo systemctl enable ps1boot.service
 
 echo "ps1boot vid installed! Reboot to see it in action!"
